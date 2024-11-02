f443a26fbd0cb68479e4edca4a36b77320edd6ea
kgsng096 <keithng.096@gmail.com>
2024-11-02 13:09:02 +0800

    Projects

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { FaHome } from "react-icons/fa";
 
@@ -17,6 +18,7 @@ export default function Home() {
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
