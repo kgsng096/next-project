32de23e568b067a7bca69c47cb5b2abc9b47f01d
kgsng096 <keithng.096@gmail.com>
2024-11-02 17:20:27 +0800

    adjusted footer bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,14 +35,14 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <a href={info.href} download target="_blank">
+            <a href={info.href} download target="_blank">
+              <div
+                key={info.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
                 <img src={info.img} alt="icons" width={20} height={20} />
-              </a>
-            </div>
+              </div>
+            </a>
           ))}
         </div>
       </div>
