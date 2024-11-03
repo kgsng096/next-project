d7d389e5479449265b77effd92edfdee0a33c24d
kgsng096 <keithng.096@gmail.com>
2024-11-03 14:30:38 +0800

    adjusted recent projects

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -10,15 +10,15 @@ const RecentProjects = () => {
         A small selection of
         <span className="text-purple"> recent projects</span>
       </h1>
-      <div className=" flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div
             key={id}
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]]"
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
           >
             <PinContainer title={link} href={link}>
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162D]">
                   <img src="/bg.png" alt="bg-img" />
                 </div>
                 <img src={img} alt={title} className="z-10 absolute bottom-0" />
@@ -34,8 +34,8 @@ const RecentProjects = () => {
                 <div className="flex items-center">
                   {iconLists.map((icon, index) => (
                     <div
-                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
-                      style={{ transform: `translateX(-${5 * index * 2}px)` }}
+                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      style={{ transform: `translateX(-${5 * index + 2}px)` }}
                     >
                       <img src={icon} alt={icon} className="p-2" />
                     </div>
